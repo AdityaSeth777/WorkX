3b2820d210b1349a96e467a39b367398d4bdfb78
Rohan Ghosh <95543022+Rohantech231@users.noreply.github.com>
2024-07-31 01:45:18 +0530

    index.html and server.js updated

---
+++ freelancer-marketplace/backend/server.js
@@ -0,0 +1,23 @@
+// backend/server.js
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/freelancer_marketplace', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
